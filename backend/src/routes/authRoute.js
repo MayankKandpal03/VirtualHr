f75509e38c2ccf7cc1ab1b6e50af171095ce6a5e
MayankKandpal03 <mayankkandpal2003@gmail.com>
2026-07-18 17:40:16 +0000

    Login Route

--- backend/src/routes/authRoute.js
+++ backend/src/routes/authRoute.js
@@ -1,8 +1,9 @@
-import { registrationController, tokenRefreshController } from "../controllers/authController.js";
+import { loginController, registrationController, tokenRefreshController } from "../controllers/authController.js";
 import { Router } from "express";
 
 const router = Router();
 
 router.post("/register", registrationController);
 router.post("/refresh-token", tokenRefreshController)
+router.post("/login", loginController)
 export default router;
